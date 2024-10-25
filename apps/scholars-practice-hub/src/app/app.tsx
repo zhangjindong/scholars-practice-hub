455166135e144fee3c4e215bacfc1ceeddcea9cd
张锦栋 <jindong.zhang@biomind.ai>
2024-10-25 16:20:51 +0800

    Initial commit
    To connect your workspace to Nx Cloud, push your repository
    to your git hosting provider and go to the following URL:
    
    https://cloud.nx.app/connect/QsIWnldBaT

---
+++ apps/scholars-practice-hub/src/app/app.tsx
@@ -0,0 +1,11 @@
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      <NxWelcome title="scholars-practice-hub" />
+    </div>
+  );
+}
+
+export default App;
